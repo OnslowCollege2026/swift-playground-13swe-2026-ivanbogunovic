949b5bd09aab91bf0ac681da510073da39386c6d
ivanbogunovic <142275358+ivanbogunovic@users.noreply.github.com>
2026-02-16 01:30:21 +0000

    Swift Is Dead

--- Sources/SwiftPlayground/main.swift
+++ Sources/SwiftPlayground/main.swift
@@ -1,26 +1,22 @@
 // MAP, FILTER & REDUCE!
 
-/*
-You are given an array of student scores out of 100. Use map, filter, and reduce to find the average score of students who passed.
+let curve_addition: Int = 5
 
-Complete these steps:
+let minimum_score_target: Int = 50
 
-Use map to curve the scores by adding 5 extra points to each student’s score.
-Use filter to keep only the passing scores (50 or more).
-Use reduce to calculate the average score of the passing students.
-*/
-
+// Set the given an array of student scores out of 100.
 let student_scores = [45, 78, 89, 32, 50, 92, 67, 41, 99, 56]
 
-let curved_scores = student_scores.map { $0 + 5 }
+// Curve the scores by adding a previously delcared integer to each of them.
+let curved_scores = student_scores.map { $0 + curve_addition }
 
-let passing_scores = curved_scores.filter { $0 >= 50 }
+// Filter out all scores below a certain target from the list.
+let passing_scores = curved_scores.filter { $0 >= minimum_score_target }
 
-let average_passing_score = passing_scores.reduce(0) {}
+// Add together all of the passing scores,
+// then divide the total by the amount of passing scores
+// to find the average passing score.
+let average_passing_score = passing_scores.reduce(0, +) / passing_scores.count
 
-// Keep passing scores: [50, 83, 94, 55, 97, 72, 104, 61]
-// Compute the average: (50 + 83 + 94 + 55 + 97 + 72 + 104 + 61) / 8 = 77
-
-/*
-When you print the correct value, you are done. Take a screenshot of your code and the result in Terminal, then upload the screenshot to the “Map, Filter, and Reduce” assignment in Google Classroom.
-*/
+// Declare the average passing score to the user.
+print("The average passing score of the assessed students is \(average_passing_score).")
